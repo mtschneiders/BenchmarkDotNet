6ae9a2cd9b81a0b305f6c55fcc01ff7a48cf0196
Adam Sitnik <adam.sitnik@gmail.com>
2017-05-24 10:24:29 +0200

    add .NET Core 2.0 samples

---
+++ samples/BenchmarkDotNet.Samples.NetCore2/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System.Reflection;
+
+namespace BenchmarkDotNet.Samples.NetCore2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(SpanFill).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+}
---
+++ samples/BenchmarkDotNet.Samples.NetCore2/SpanFill.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace BenchmarkDotNet.Samples.NetCore2
+{
+    public class SpanFill
+    {
+        private byte[] array;
+
+        [Params(10, 100, 1000)]
+        public int Length { get; set; }
+
+        [Setup]
+        public void Setup()
+        {
+            array = new byte[Length];
+        }
+
+        [Benchmark()]
+        public void TestSpanFillByte()
+        {
+            var span = new Span<byte>(array);
+            span.Fill(default(byte));
+        }
+    }
+}
